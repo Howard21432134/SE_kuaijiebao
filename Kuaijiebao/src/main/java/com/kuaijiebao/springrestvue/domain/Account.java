1b78365d660adf7e3d726ff1634b2a654ab5204f
aikawasatoshi <>
2018-07-10 11:26:25 +0800

    Impl. MyAccount API

--- Kuaijiebao/src/main/java/com/kuaijiebao/springrestvue/domain/Account.java
+++ Kuaijiebao/src/main/java/com/kuaijiebao/springrestvue/domain/Account.java
@@ -1,4 +1,25 @@
 package com.kuaijiebao.springrestvue.domain;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "account")
 public class Account {
+    private String username;
+    @Id
+    @Column(name="user_id")
+    private Long id;
+    private String password;
+
 }
